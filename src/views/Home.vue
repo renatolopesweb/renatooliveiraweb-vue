68e7a358b7aaf9c67a365c8ffb9f3d334c702879
= <=>
2021-06-21 15:04:54 -0300

    Deploy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,6 +30,8 @@ export default {
 $color-primary: #8c52ff;
 $t-def: 0.3s;
 
+
+
 section{
   grid-template-columns: repeat(3, 1fr);
   article{
@@ -39,7 +41,10 @@ section{
     transition: $t-def;
   }
 article:nth-child(2) {
+  box-shadow: 1px 0px 6px 0px rgba($color: #ccc, $alpha: 0.8);
   background: rgba($color: #f0f0f0, $alpha: 0.6);
+    // box-shadow:  20px 20px 60px rgba($color: #ccc, $alpha: 0.4),
+    //          -20px -20px 60px #ffffff;
 }
 }
 
