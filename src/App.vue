b5ce276f3c371d59c332aa4ac91eabc44cf60978
= <=>
2021-07-14 02:38:11 -0300

    Deploy

--- src/App.vue
+++ src/App.vue
@@ -63,11 +63,12 @@ section {
 
 /* DESIGN */
 .card {
+  background: #fff;
   border-radius: 2px;
   // border: 1px solid rgba($color: #ccc, $alpha: 0.3);
-  // box-shadow: 0 2px 2px 0 rgb(0 0 0 / 25%), 0 3px 1px -2px rgb(0 0 0 / 5%),
-  //   0 1px 5px 0 rgb(0 0 0 / 5%);
-  box-shadow: 20px 20px 60px #ccc, -20px -20px 60px #ffffff;
+  box-shadow: 0 2px 2px 0 rgb(0 0 0 / 5%), 0 3px 1px -2px rgb(0 0 0 / 5%),
+    0 1px 5px 0 rgb(0 0 0 / 15%);
+  // box-shadow: 20px 20px 60px #ccc, -20px -20px 60px #ffffff;
   // box-shadow: 1px 25px 40px rgba($color: #666, $alpha: 0.1);
   transition: $t-def;
   // &:hover {
