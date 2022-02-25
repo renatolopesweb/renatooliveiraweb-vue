e4ebc2bf4bd6d2a15161dce5741b7f4fc57ce093
= <=>
2022-02-24 23:12:16 -0300

    Deploy 2022

---
+++ src/views/About.vue
@@ -0,0 +1,106 @@
+<template>
+    <section>
+      <p>
+        Meu dia a dia de trabalho:
+      </p>
+      <div class="skills">
+        <div>
+          <h2><i class="fas fa-code" aria-hidden="true"></i> Code</h2>
+          <ul>
+            <li>javaScript</li>
+            <li>typeScript</li>
+            <li>HTML5</li>
+            <li>Sass / CSS</li>
+          </ul>
+        </div>
+        <div>
+          <h2><i class="fas fa-tools" aria-hidden="true"></i> Framework</h2>
+          <ul>
+            <li>Angular</li>
+            <li>Electron</li>
+            <li>Ionic</li>
+            <li>Vue</li>
+          </ul>
+        </div>
+        <div>
+          <h2><i class="fas fa-eye" aria-hidden="true"></i> Visual</h2>
+          <ul>
+            <li>XD</li>
+            <li>Photoshop</li>
+            <li>Premiere</li>
+          </ul>
+        </div>
+        <div>
+          <h2><i class="fas fa-bullhorn" aria-hidden="true"></i> Marketing</h2>
+          <ul>
+            <li>Wordpress</li>
+            <li>E-mail mkt</li>
+          </ul>
+        </div>
+        <!-- <div v-for="(item, article) in proj" :key="article">
+            <h2><i :class="item.ico" aria-hidden="true"></i> {{ item.categ.title }}</h2>
+            <ul>
+              <li>{{ item.lists }}</li>
+            </ul>
+        </div> -->
+      </div>
+    </section>
+</template>
+
+
+<script>
+// export default {
+//   data() {
+//     return {
+//       proj: [
+//         {
+//           categ: { title: "Code" },
+//           ico: "fas fa-code",
+//           lists: [
+//             {
+//               bullets: ["lol"],
+//             },
+//           ],
+//         },
+//       ],
+//     };
+//   },
+// };
+</script>
+
+
+<style lang="scss" scoped>
+.skills {
+  display: flex;
+  gap: 100px;
+  h2 {
+    font-size: 1.1em;
+    margin: 15px 0;
+    text-transform: uppercase;
+  }
+  li {
+    font-size: 1.1em;
+    margin-bottom: 7px;
+  }
+}
+@media (max-width: 1000px) {
+  section{
+    padding: 25px;
+  }
+}
+@media (max-width: 800px) {
+  .skills {
+    gap: 50px;
+    padding-bottom: 50px;
+  }
+}
+@media (max-width: 650px) {
+  .skills {
+    flex-direction: column;
+  }
+}
+
+// .skills > div :nth-child(1) {
+//   text-align: center;
+// }
+</style>
---
+++ src/views/Links.vue
@@ -0,0 +1,136 @@
+<template>
+  <section>
+    <article v-for="(item, article) in projects.front" :key="article">
+      <figure>
+        <img :src="item.screen" :alt="item.screen" />
+      </figure>
+      <div>
+        <h2>{{ item.intro }}</h2>
+        <h3>{{ item.title }}</h3>
+        <p>
+          {{ item.description }}
+        </p>
+        <p>
+          <a class="hover" target="_blank" :href="item.url"
+            ><i class="fas fa-external-link-alt" aria-hidden="true"></i>Link do
+            projeto</a
+          >
+        </p>
+      </div>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: {
+        front: [
+          {
+            intro: "Website",
+            title: "Clone Starbucks",
+            description: "Crédito de design para Nicepage",
+            url: "https://renato7oliveira.github.io/experimentos/Starbucks/src/",
+            screen: require("../assets/images/thumb-links-starbucks.jpg"),
+          },
+          {
+            intro: "Landpage",
+            title: "Desafio CSS-Grid",
+            description:
+              "Recebi este design em Photoshop e estruturei com Sass + CSS-Grid + fontAwesome",
+            url: "https://renato7oliveira.github.io/experimentos/Grid_CSS_Template_Canyon/src/",
+            screen: require("../assets/images/thumb-links-canyon.jpg"),
+          },
+          {
+            intro: "Website",
+            title: "Clone Tesla (Header)",
+            description: "Usando Flex-box e CSS3 animate",
+            url: "https://renato7oliveira.github.io/experimentos/Tesla/src/",
+            screen: require("../assets/images/thumb-links-tesla.jpg"),
+          },
+          {
+            intro: "Hotsite",
+            title: "Street Fighter Tribute (oldSchool)",
+            description: "Projeto interativo feito com Adobe Edge, demora um pouco para carregar",
+            url: "https://renato7oliveira.github.io/street-fighter-audio-project/",
+            screen: require("../assets/images/thumbs-links-fighter.jpg"),
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
+
+
+
+<style lang="scss" scoped>
+.head :nth-child(3) {
+  text-align: center;
+}
+
+article {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-areas: "figure figure description description";
+  grid-gap: 100px;
+  align-content: center;
+  align-items: center;
+  margin-bottom: 50px;
+  padding: 25px 0;
+  figure {
+    grid-area: figure;
+    img {
+      width: 100%;
+    }
+  }
+  div {
+    grid-area: description;
+  }
+  h2 {
+    color: #999;
+    font-size: 1em;
+    font-weight: 500;
+    text-transform: uppercase;
+  }
+  h3 {
+    font-size: 1.6em;
+  }
+  p {
+    line-height: 30px;
+    margin-bottom: 5px;
+    margin-top: 5px;
+  }
+  i {
+    font-size: 0.7em;
+    margin-right: 5px;
+  }
+  a {
+    color: var(--color-primary);
+  }
+
+  span {
+    color: #ff5f04;
+  }
+}
+
+@media (max-width: 1200px) {
+  article {
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      "figure"
+      "description";
+    grid-gap: 0px;
+    padding: 0;
+    img {
+      margin-bottom: 10px;
+    }
+    div {
+      padding: 0 10px;
+      margin-bottom: 25px;
+    }
+  }
+}
+
+</style>
