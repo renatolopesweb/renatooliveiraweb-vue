e942ffbce3945f42031c945964353297d14b8173
= <=>
2022-03-13 14:22:04 -0300

    Deploy

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,23 +1,25 @@
 <template>
   <article>
     <router-link to="/">
-      <h2>Página não encontrada</h2>
-      <div class="ghost" title="Arraste-me pra Home!!!">👻</div>
+        <h2>Página não encontrada</h2>
+        <div class="ghost" title="Arraste-me pra Home!!!">👻</div>
     </router-link>
   </article>
 </template>
 
 <style lang="scss" scoped>
-
 article {
+  display: grid;
+  align-content: center;
+  height: calc(100vh - 90px);
   text-align: center;
-  a{
+  a {
     color: var(--color-primary);
   }
 }
 
 .ghost {
-  font-size: 2em;
+  font-size: 1.8em;
   animation: ghost 0.5s ease-in-out alternate infinite;
   &:hover {
     animation-play-state: paused;
