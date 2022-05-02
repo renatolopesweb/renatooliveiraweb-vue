1c46b4e5c43f7839e602a9636a45e0d385b7c1b3
= <=>
2022-05-02 00:05:33 -0300

    Deploy

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -1,15 +1,20 @@
 <template>
   <section>
     <h1>Links</h1>
-    <article
-      v-for="(item, article) in projects.front"
-      :key="article"
-      class="card"
-    >
-      <span>{{ item.intro }}</span>
-      <h2><a :href="item.url" target="_blank">{{ item.title }}</a></h2>
-      <p>{{ item.stack }}</p>
-    </article>
+    <h2>Minhas Redes Sociais</h2>
+    <ul>
+      <li v-for="item in social" :key="item.id">
+        <a :href="item.url" target="_blank" :title="item.title"
+          ><i :class="item.img" aria-hidden="true"></i> {{ item.title }}</a
+        >
+      </li>
+    </ul>
+    <!-- <h2>Experimentos Web</h2>
+    <ul>
+      <li v-for="(item, article) in projects.front" :key="article">
+        <a :href="item.url" target="_blank">{{ item.title }}</a>
+      </li>
+    </ul> -->
   </section>
 </template>
 
@@ -21,64 +26,61 @@ export default {
       projects: {
         front: [
           {
-            intro: "item.categ",
-            title: "Em atualização",
-            stack: "item.stack",
-            url: "https://www.renatooliveiraweb.com/#/links",
+            title: "Landpage CSS-Grid",
+            url: "https://renato7oliveira.github.io/experimentos/Grid_CSS_Template_Canyon/src/",
+          },
+          {
+            title: "Header Tesla",
+            url: "https://renato7oliveira.github.io/experimentos/Tesla/src/",
+          },
+          {
+            title: "E-mail Marketing VIVO (HTML 1.0)",
+            url: "https://renato7oliveira.github.io/experimentos/Email_Mkt/Vivo/index.html",
+          },
+          {
+            title: "Street Fighter Animate (Old School)",
+            url: "https://renato7oliveira.github.io/street-fighter-audio-project/",
           },
         ],
       },
+      social: [
+        {
+          title: "Linkedin",
+          img: "fab fa-linkedin",
+          url: "https://www.linkedin.com/in/renatooliveiraweb",
+        },
+        {
+          title: "Instagram",
+          img: "fab fa-instagram",
+          url: "https://www.instagram.com/renatooliveiraweb",
+        },
+        {
+          title: "Github",
+          img: "fab fa-github-alt",
+          url: "https://github.com/renato7oliveira",
+        },
+      ],
     };
   },
 };
 </script>
 
-
 <style scoped lang="scss">
-h1 {
-  grid-row: 1;
-  grid-column: 1 / -1;
-  margin-bottom: 0px;
+ul {
+  margin-bottom: 75px;
 }
-section {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 25px;
-  padding-bottom: 25px;
-}
-article {
-  background: #fff;
-  padding: 25px;
-  h2 , a {
+li {
+  font-size: 1.1em;
+  margin-top: 15px;
+  i {
+    text-align: center;
+    width: 20px;
+  }
+  a {
     color: var(--color-primary);
-    margin: 15px 0;
-    &:hover{
+    &:hover {
       text-decoration: underline;
     }
   }
-  p {
-    font-weight: 700;
-  }
 }
-@media (max-width: 900px) {
-  h1{
-    margin-bottom: 25px;
-  }
-  section {
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: 0 25px;
-    padding-bottom: 75px;
-  }
-  article {
-    margin-bottom: 25px;
-    padding: 25px;
-  }
-}
-@media (max-width: 600px) {
-  section {
-    grid-template-columns: 1fr;
-  }
-}
-
-
 </style>
